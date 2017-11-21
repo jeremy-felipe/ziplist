84b74fb12e39be02b60e408ffacdf40b108fe359
jeremy-felipe <jeremyhonolulu@gmail.com>
2017-11-20 22:08:22 -1000

    second set of tests working

--- ziplist.test.js
+++ ziplist.test.js
@@ -8,10 +8,17 @@ describe('zipList', function () {
     it('should return 6 for zipList length', function () {
       chai.expect(zipList(testNums1, testNums2).length).to.equal(6);
     });
+    it('should be in the right order', function () {
+      chai.expect(zipList(testNums1, testNums2).toString() === [1, 'a', 2, 'b', 3, 'c'].toString()).to.equal(true);
+    });
   });
   describe('zipListTheSimpleWay()', function () {
     it('should return 6 for zipListTheSimpleWay length', function () {
       chai.expect(zipListTheSimpleWay(testNums1, testNums2).length).to.equal(6);
+    });
+    it('should be in the right order', function () {
+      chai.expect(zipListTheSimpleWay(testNums1, testNums2).toString()
+          === [1, 'a', 2, 'b', 3, 'c'].toString()).to.equal(true);
     });
   });
 });
